fd1ec735f5609d7cb88632461f509d99016f5d27
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-03-21 19:07:26 -0300

    FIRST

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nome = ref('Nome')
+
+const contador = ref(1)
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>Bem vindo {{ nome }}</h1>
+    <p><input type="text" v-model="nome" /></p>
+    <p v-if="nome.length > 5">Nome longo</p>
+    <p v-else>Nome curto</p>
+    <p>{{ contador }} <button @click="incrementar">Incrementar</button></p>
+  </div>
+</template>
